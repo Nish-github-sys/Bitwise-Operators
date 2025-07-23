afb55c0c06ec1275086d19b775d6d3cea623e1c3
Nishka Ranadive <nishka.ranadive@outlook.com>
2025-07-23 23:56:11 +0530

    Update exp-4.cpp

--- exp-4.cpp
+++ exp-4.cpp
@@ -34,28 +34,6 @@ int main() {
 }
 
 
-//Program-2
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n=12;
-    int bit_reset, bit_set, bit_set_o, bit_reset_o;
-    
-    cout<<"Enter bit position to be set: ";
-    cin>>bit_set;
-    bit_set_o=n|(1<<bit_set);
-    cout<<"Set_output:"<<bit_set_o<<endl;
-    
-    cout<<"Enter bit position to be reset: ";
-    cin>>bit_reset;
-    bit_reset_o=n&(~(1<<bit_reset));
-    cout<<"Reset_output:"<<bit_reset_o<<endl;
-
-    return 0;
-}
-
-
 /*==========OUTPUTS==========
 -->Program-1:
 Bitwise AND:2
@@ -67,9 +45,6 @@ Left Shift_a:32
 Right Shift_a:0
 Left Shift_b:112
 Right Shift_b:0
+*/
 
--->Program-2:
-Enter bit position to be set: 2
-Set_output:12
-Enter bit position to be reset: 3
-Reset_output:4*/
+
